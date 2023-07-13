1694aec749cbe2e86036ca6f37dc3c2c747e901d
Guilherme Baia Venâncio <guilhermebaiav@gmail.com>
2023-07-13 01:25:52 -0300

    Add Tailwind and Header

---
+++ myVeryownWebsite/src/components/sections/LandingSec.jsx
@@ -0,0 +1,24 @@
+export function LandingSec() {
+    return (
+        <div>
+            <header className="flex justify-between px-10 py-2 border-b items-center         ">
+                <p className="font-bold text-amber-900">GUI.BAIA</p>
+                <ul className="flex flex-row space-x-5 text-xs">
+                    <li><a href="#">Habilidades</a></li>
+                    <li><a href="#">Projetos</a></li>
+                    <li><a href="#">Currículo</a></li>
+                    <li><a href="#">Contatos </a></li> 
+
+                </ul>
+            </header>
+        </div>
+    );
+}
+
+/* Align Items vs Justify Content
+Align: cross axis
+Justify: main axis
+
+
+
+*/
