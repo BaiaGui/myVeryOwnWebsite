1fc545a252addd5f9dcc9031e4e511d9835a782c
Guilherme Baia Venâncio <guilhermebaiav@gmail.com>
2023-07-18 20:40:54 -0300

    Skill Section Blocking

--- myVeryownWebsite/src/App.jsx
+++ myVeryownWebsite/src/App.jsx
@@ -1,12 +1,17 @@
 import './styles/App.css'
 import { LandingSec } from './components/sections/LandingSec'
+import { SkillsSec } from './components/sections/SkillsSec'
 
 function App() {
 
   return (
-    <>
+    <div className='flex flex-col'>
       <LandingSec/>
-    </>
+      <SkillsSec>
+        <p>HTML</p>
+        <p>CSS</p>
+      </SkillsSec>
+    </div>
   )
 }
 
