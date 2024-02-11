e56d639e37d86da36726eca77c04f7593c1986e8
Guilherme Baia <guilhermebaiav@gmail.com>
2024-02-11 15:54:03 -0300

    adds new project card component

---
+++ myVeryownWebsite/src/components/ProjectCard.jsx
@@ -0,0 +1,34 @@
+import * as HoverCard from '@radix-ui/react-hover-card';
+
+export function ProjectCard({ project }) {
+
+    let projectTags = project.tags.map(tag => <Tags key={crypto.randomUUID} content={tag} />)
+
+    return (
+        <HoverCard.Root openDelay={100} closeDelay={100}>
+            <HoverCard.Trigger asChild>
+                <a href={project.deployLink}><img src={project.thumbnail} alt="" className='aspect-video w-96 rounded-md' /></a>
+            </HoverCard.Trigger>
+            <HoverCard.Portal>
+                <HoverCard.Content asChild>
+                    <div className='bg-white p-5 mt-2 w-[400px] shadow-sm rounded-md'>
+                        {project.description}
+                        <div className='flex gap-1 mt-5'>
+                            {projectTags}
+                        </div>
+
+                    </div>
+                </HoverCard.Content>
+            </HoverCard.Portal>
+        </HoverCard.Root>
+    )
+}
+
+export function Tags({ content }) {
+    return (
+        <div className='px-3 py-1 bg-amber-100 border text-amber-800 border-amber-300 text-xs rounded-full'>
+            {content}
+        </div>
+    )
+}
+
--- myVeryownWebsite/src/components/sections/ProjectsSec.jsx
+++ myVeryownWebsite/src/components/sections/ProjectsSec.jsx
@@ -3,52 +3,45 @@ import plannerThumb from '../../assets/plannerThumb.webp'
 import vertexaThumb from '../../assets/vertexaThumb.webp'
 import external from '../../assets/external.svg'
 import { SecTitle } from '../SecTitle';
+import { ProjectCard } from '../ProjectCard';
 
 export function ProjectsSec(){
+
+    let projects = [
+        {
+            "thumbnail": plannerThumb,
+            "deployLink": "https://baiaplanner.netlify.app",
+            "description": 'Um planner semanal para auxiliar na organização de tarefas da semana. Feito com React e Tailwind',
+            "tags": ['React', 'Tailwind']
+        },
+        {
+            "thumbnail": plannerThumb,
+            "deployLink": "https://baiaplanner.netlify.app",
+            "description": 'Um planner semanal para auxiliar na organização de tarefas da semana. Feito com React e Tailwind',
+            "tags": ['React', 'Tailwind']
+        },
+        {
+            "thumbnail": plannerThumb,
+            "deployLink": "https://baiaplanner.netlify.app",
+            "description": 'Um planner semanal para auxiliar na organização de tarefas da semana. Feito com React e Tailwind',
+            "tags": ['React', 'Tailwind']
+        },
+    ]
+
+    const cards = projects.map(project => <ProjectCard key={crypto.randomUUID} project={project}/> )
+
+
+
     return (
         <div className="flex flex-col items-center lg:px-64 py-10 bg-amber-400/5" id='ProjectSecId'>
-            <SecTitle>{"Projetos"}</SecTitle>
-            <ProjectCard side={"l"} title={"Pop Tac Toe"} imgUrl={popThumb} link={"https://baiagui.github.io/pop-tac-toe/"}>
-                Jogo da velha baseado nos estilo pop art feito em HTML, CSS e Javascript
-            </ProjectCard>
-            <ProjectCard side={"r"} title={"Vertexa"} imgUrl={vertexaThumb} link={"https://vertexa.com.br/"}>
-                Site institucional para a empresa Vertexa utilizando HTML, CSS e Javascript
-            </ProjectCard>
-            <ProjectCard side={"l"} title={"myPlanner"} imgUrl={plannerThumb} link={"https://baiaplanner.netlify.app"}>
-                Um planner semanal para auxiliar na organização de tarefas da semana. Feito com React e Tailwind
-            </ProjectCard> 
+             <SecTitle>{"Projetos"}</SecTitle>
+             <div className='mt-5 flex gap-2'>
+             {cards}
+             </div>
+             
         </div>
     );
 }
 
 
 
-function ProjectCard({imgUrl, title, children, side, link}){
-    
-
-    if(side=="l"){
-
-        return(
-            <div className="flex flex-col items-center w-full mt-24 p-5 lg:flex-row lg:w-fit">
-                <img src={imgUrl} alt="" className=" w-96 rounded-md"/>
-                <div className="flex flex-col items-center mt-5 lg:items-start lg:ml-5">
-                <a href={link} target="_blank" className="flex font-bold text-xl hover:underline text-zinc-700">{title}<img src={external} className="pl-2 w-5"/></a>
-                <p className="text-justify">{children}</p>
-                </div>
-            </div> 
-            );
-    }
-    else{
-        return(
-            <div className="flex flex-col items-center w-full mt-24 p-5 lg:flex-row-reverse lg:w-fit">
-               <img src={imgUrl} alt="" className="w-96 rounded-md "/>
-                <div className="flex flex-col items-center mt-5 lg:items-end lg:mr-5">
-                <a href={link} target="_blank" className="flex font-bold text-xl hover:underline text-zinc-700">{title}<img src={external} className="pl-2 w-5"/></a>
-                <p className="text-justify lg:text-right">{children}</p>
-                </div>
-            </div>
-        );
-    }
-    
-   
-}
