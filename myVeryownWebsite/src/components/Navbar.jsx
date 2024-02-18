dbd0f76c4c310bd879731f6979a5d237f7443680
Guilherme Baia <guilhermebaiav@gmail.com>
2024-02-18 18:04:15 -0300

    adjust hero layout

--- myVeryownWebsite/src/components/Navbar.jsx
+++ myVeryownWebsite/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 
 export function Navbar() {
 
-    const [isOpen, setIsOpen] = useState(true)
+    const [isOpen, setIsOpen] = useState(false)
 
     function handleMenuToggle(){
         setIsOpen(!isOpen)
@@ -12,9 +12,9 @@ export function Navbar() {
 
     return (
 
-        <header className="border-b flex flex-col sm:flex-row sm:justify-between sm:items-center">
-            <div className=" flex justify-between items-center px-4 py-3">
-                <p className="font-bold text-amber-900 text-xl">GUI.BAIA</p>
+        <header className="border-b flex flex-col sm:flex-row sm:justify-between sm:items-center px-10">
+            <div className=" flex justify-between items-center px-2 py-3">
+                <p className="font-bold text-orange-700 text-2xl">GUI.BAIA</p>
                 <div className="sm:hidden">
                     <button onClick={handleMenuToggle} className="block text-amber-900 focus:outline-none">
                         <Menu className={isOpen?"hidden":"block"} />
@@ -23,10 +23,10 @@ export function Navbar() {
                 </div>
             </div>
             <nav className={ (isOpen ? "block " : "hidden ") + "bg-amber-800/10 backdrop-blur-lg flex flex-col px-2 py-3 sm:block sm:bg-transparent sm:backdrop-blur-none sm:justify-center sm:align-center"}>
-                <a className=" font-bold text-amber-950 hover:bg-amber-950/5 rounded-md px-2 py-1 sm:px-3 sm:py-3" href="#LandingSecId">Home</a>
-                <a className="font-bold text-amber-950 hover:bg-amber-950/5 rounded-md px-2 py-1 sm:px-3 sm:py-3" href="#SkillsSecId">Habilidades</a>
-                <a className="font-bold text-amber-950 hover:bg-amber-950/5 rounded-md px-2 py-1 sm:px-3 sm:py-3" href="#ProjectSecId">Projetos</a>
-                <a className="font-bold text-amber-950 hover:bg-amber-950/5 rounded-md px-2 py-1 sm:px-3 sm:py-3" href="#ContactSecId">Contatos </a>
+                <a className=" font-semibold text-stone-800 text-sm hover:bg-amber-950/5 rounded-md px-2 py-1 sm:px-3 sm:py-3" href="#LandingSecId">Home</a>
+                <a className="font-semibold text-stone-800 text-sm hover:bg-amber-950/5 rounded-md px-2 py-1 sm:px-3 sm:py-3" href="#SkillsSecId">Habilidades</a>
+                <a className="font-semibold text-stone-800 text-sm hover:bg-amber-950/5 rounded-md px-2 py-1 sm:px-3 sm:py-3" href="#ProjectSecId">Projetos</a>
+                <a className="font-semibold text-stone-800 text-sm hover:bg-amber-950/5 rounded-md px-2 py-1 sm:px-3 sm:py-3" href="#ContactSecId">Contatos </a>
             </nav>
         </header>
     )
--- myVeryownWebsite/src/components/sections/LandingSec.jsx
+++ myVeryownWebsite/src/components/sections/LandingSec.jsx
@@ -6,13 +6,6 @@ import { Navbar } from '../Navbar';
 export function LandingSec() {
     return (
         <>  
-            {/* <div className="absolute -z-10 w-full h-full">
-                <div className='bg-red-600/20 w-48 h-48 rounded-full absolute top-2/4 left-72'></div>
-                <div className='bg-amber-600/20 w-72 h-72 rounded-full absolute top-44 left-1/3'></div>
-                <div className='bg-fuchsia-800/20 w-96 h-96 rounded-full absolute top-9 left-2/3'></div>
-                <div className='bg-sky-500/20 w-1/4 h-2/4 rounded-full -top-36 -left-14 absolute'></div>
-                <div className='bg-green-500/20 w-1/4 h-2/4 rounded-full top-3/4 left-3/4 absolute'></div>
-            </div> */}
             <div className="absolute w-full h-screen -z-10 blur-2xl">
             <img src={bgBlur} className="w-full h-full bg-cover opacity-75" alt="" />
             </div>
@@ -24,8 +17,12 @@ export function LandingSec() {
                         <img src={profile} alt="Selfie" className="flex rounded-full mb-5 md:mb-0 md:w-2/3" />
                     </div>
                     <div className="flex flex-col justify-center items-center  md:items-start md:w-1/2">
-                        <h1 className="w-fit text-center font-bold text-xl md:text-start md:text-2xl lg:text-4xl text-stone-800">Olá, meu nome é Guilherme Baia. Sou desenvolvedor Front-end e Web Designer</h1>
-                        <h2 className="w-fit mt-4 text-center md:text-start text-md text-stone-600">Minha missão é, através da tecnologia e do meu trabalho, causar impactos positivos na sociedade melhorar a qualidade de vida de todos. Conheça mais sobre meu trabalho! </h2>
+                        <h1 className="w-fit text-center font-bold text-3xl md:text-start md:text-6xl xl:text-8xl text-stone-800 tracking-tighter">Guilherme Baia</h1>
+                        <h2 className="bg-gradient-to-bl  font-semibold mb-9 from-red-600 to-orange-600 bg-clip-text text-transparent font-serif italic text-center text-xl md:text-3xl md:text-start">Desenvolvedor Web Full-Stack</h2>
+                        <h3 className="text-justify w-fit mt-4 md:text-start text-md text-stone-600">
+                            Oi! Sou desenvolvedor web full-stack e sou apaixonado por criar websites e aplicações.
+                            Eu acredito que a tecnologia tem o poder de melhorar a vida das pessoas e quero poder contribuir com isso através do meu trabalho.<br/>Estou sempre à procura de novos desafios para me aprimorar como desenvolvedor e estou aqui para ajudar você com suas necessidades tecnológicas. Sinta-se à vontade para explorar mais sobre meu trabalho e como posso colaborar com seus projetos!
+                        </h3>
                         <a href="#SkillsSecId"><button className="text-md border font-bold text-stone-800 px-20 py-6 rounded-md mt-9 bg-stone-100/50 border-amber-900/20 hover:scale-105 ease-in-out duration-100">Explorar</button></a>
                     </div>
                 </div>
